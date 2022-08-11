d8e28183e9c80422a46ca8f38677c755ba2c66bd
akshays-bw <akshay.s@brainwired.in>
2022-08-11 20:58:21 +0530

    fix:auth

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -5,11 +5,18 @@ import { useEffect, useState } from 'react'
 import AppLayout from '../components/layout'
 import AppAuth from '../features/Auth/Auth'
 import { AppAuthentication } from '../lib/authentication'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 function withAuth(Component: NextComponentType) {
+  Router.events.on('routeChangeStart', (url) => {
+    console.log(`Loading: ${url}`)
+    NProgress.start()
+  })
+  Router.events.on('routeChangeComplete', () => NProgress.done())
+  Router.events.on('routeChangeError', () => NProgress.done())
   const Auth = (props: any) => {
     const { asPath } = useRouter()
     const [authState, setAuthState] = useState<'s' | 'l' | 'f'>('l')
-
     useEffect(() => {
       const isLoggedIn = new AppAuthentication().isUserLogined()
       if (isLoggedIn) {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,19 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../store'
-import Router from 'next/router'
-import NProgress from 'nprogress'
-
-Router.events.on('routeChangeStart', (url) => {
-  console.log(`Loading: ${url}`)
-  NProgress.start()
-})
-Router.events.on('routeChangeComplete', () => NProgress.done())
-Router.events.on('routeChangeError', () => NProgress.done())
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <NextNProgress
+        color="#e94f4f"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+        showOnShallow={true}
+      />
+
       <Component {...pageProps} />
     </Provider>
   )
